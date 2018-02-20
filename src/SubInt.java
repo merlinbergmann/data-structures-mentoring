f085ba8dcc3ff394ef59ec166d288d86d4a0467f
merlinbergmann <hello@merlin.codes>
2018-02-20 15:53:40 +0100

    SubInt solved

---
+++ src/SubInt.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubInt {
+  public static void main(String[] args) {
+    //  Create a function that takes a number and a list of numbers as a parameter
+    //  Returns the indeces of the numbers in the list where the first number is part of
+    //  Returns an empty list if the number is not part any of the numbers in the list
+
+    //  Example:
+    System.out.println(subInt(1, new int[] {1, 11, 34, 52, 61}));
+    //  should print: `[0, 1, 4]`
+    System.out.println(subInt(9, new int[] {1, 11, 34, 52, 61}));
+    //  should print: '[]'
+  }
+
+  private static List subInt(int i, int[] ints) {
+    List<Integer> indexes = new ArrayList<>();
+
+    for (int j = 0; j < ints.length; j++) {
+      if (Integer.toString(ints[j]).contains(Integer.toString(i))) {
+        indexes.add(j);
+      }
+    }
+
+    return indexes;
+  }
+}
